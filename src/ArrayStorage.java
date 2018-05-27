e32d3058d0dde3589e1118d44df0b1f504dcd3cb
Ilya Zhynko <keshondzin@gmail.comgit configgit config --global user.email keshondzin@gmail.com>
2018-05-27 22:57:24 +0300

    firstTask

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,66 @@
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        for (Resume resume : storage)
+            if (resume != null)
+                resume = null;
+        size = 0;
     }
 
     void save(Resume r) {
+        for (Resume resume : storage) {
+            if (resume == null)
+                break;
+            if (resume.uuid.equals(r.uuid))
+                return;
+        }
+        if (size < storage.length) {
+            storage[size] = r;
+            size++;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume r = null;
+        for (Resume resume : storage) {
+            if (resume == null)
+                break;
+            if (resume.uuid.equals(uuid)) {
+                r = resume;
+                break;
+            }
+        }
+        return r;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                size--;
+                System.arraycopy(storage, i + 1, storage, i, storage.length - i - 1);
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] r = new Resume[size];
+        System.arraycopy(storage, 0, r, 0, size);
+        return r;
     }
 
     int size() {
-        return 0;
+        return size;
     }
+
 }
